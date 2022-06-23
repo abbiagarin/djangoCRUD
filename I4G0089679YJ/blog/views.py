3ac54e08c80b2670694ea238e5634634c98eb1b1
Abigail Agarin <abigail.agarin@iisysgroup.com>
2022-06-23 23:20:59 +0100

    Django CRUD Task

---
+++ I4G0089679YJ/blog/views.py
@@ -0,0 +1,36 @@
+from audioop import reverse
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Post
+
+# from djangoModels.I4G0089679YJ.blog.models import Post
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'templates/blog/post_list.html'
+
+class PostCreateView(CreateView):
+   model = Post
+   fields = "__all__"
+   success_url = reverse_lazy("blog:all")
+   template_name = 'templates/blog/post_form.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'templates/blog/post_detail.html'
+
+class PostUpdateView(UpdateView):
+   model = Post
+   fields = "__all__"
+   success_url = reverse_lazy("blog:all")
+   template_name = 'templates/blog/post_form.html'    
+
+class PostDeleteView(DeleteView):
+   model = Post
+   fields = "__all__"
+   success_url = reverse_lazy("blog:all") 
+   template_name = 'templates/blog/post_confirm_delete.html'   
