50a32f0f31e7defe35c2bc0f34f64b119b5f17a5
Abigail Agarin <abigail.agarin@iisysgroup.com>
2022-06-24 14:14:30 +0100

    modifications on views.py and templates folder

--- I4G0089679YJ/blog/views.py
+++ I4G0089679YJ/blog/views.py
@@ -11,26 +11,26 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = 'templates/blog/post_list.html'
+    template_name = 'blog/post_list.html'
 
 class PostCreateView(CreateView):
    model = Post
    fields = "__all__"
    success_url = reverse_lazy("blog:all")
-   template_name = 'templates/blog/post_form.html'
+   template_name = 'blog/post_form.html'
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'templates/blog/post_detail.html'
+    template_name = 'blog/post_detail.html'
 
 class PostUpdateView(UpdateView):
    model = Post
    fields = "__all__"
    success_url = reverse_lazy("blog:all")
-   template_name = 'templates/blog/post_form.html'    
+   template_name = 'blog/post_form.html'    
 
 class PostDeleteView(DeleteView):
    model = Post
    fields = "__all__"
    success_url = reverse_lazy("blog:all") 
-   template_name = 'templates/blog/post_confirm_delete.html'   
+   template_name = 'blog/post_confirm_delete.html'   
